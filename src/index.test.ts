435e5107f5f3ff3ab1e11d3b84738f9f17a6b327
Adam Dornford <adam.dornford@gmail.com>
2019-12-20 10:59:03 -0500

    update compilation and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,223 +2,204 @@
  * @jest-environment node
  */
 
+import { iex } from "./index";
 
-import * as iex from "./index";
+import {
+  AccountMetaData, AdvancedStats, Auction, BalanceSheet, Book, CashFlowStatement,
+  CeoCompensation,  Company, DEEPBook, DEEPOfficialPrice, DEEPTrade, DelayedQuote,
+  Dividends, Earnings, EarningsToday, EndOfDay, EndOfDayCloseOnly,
+  Estimates, Financials, IEXSymbol, IncomeStatement, Intraday, KeyStats, Logo,
+  MarketSymbol, MarketVolume, MessageUsage, NewsItem, OHLC, PreviousDay, PriceTarget,
+  Quote, RulesUsage, SectorPerformance, SocialSentiment, Splits, TOPS, TOPSLast,
+  UsageType, VolumeByVenue
+} from "./types";
 
+// const defaultHandler = (evt: MessageEvent): void => {
+//   // tslint:disable-next-line: no-console
+//   console.log(evt.data);
+// }
 
-test("advancedStats", async () => {
-  expect(await iex.advancedStats("AAPL")).toBeInstanceOf(iex.AdvancedStats)
+// define jest type checker
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeArrayInstanceOf<T>(T: T): CustomMatcherResult;
+      toBeArrayTypeOf<T>(T: T): CustomMatcherResult;
+      toBeTypeOf<T>(T: T): CustomMatcherResult;
+    }
+  }
+}
+
+const isType = (received: any, argument: any): boolean => {
+  const initialType = typeof received;
+  const type = initialType === "object" ? (Array.isArray(received) ? "array" : initialType) : initialType;
+  return type === argument;
+};
+
+expect.extend({
+  toBeArrayInstanceOf: (received: object[], argument) => {
+    return received.reduce((prev, curr) => prev && curr instanceof argument, true) ? {
+      message: () => `expected ${received} to be type ${argument.constructor}`,
+      pass: true
+    } : {
+      message: () => `expected ${received} to be type ${argument.constructor}[]`,
+      pass: false
+    };
+  },
+  toBeArrayTypeOf: (received: object[], argument) => {
+    return received.reduce((prev, curr) => prev && isType(curr, argument), true) ? {
+      message: () => `expected received to be type ${argument}`,
+      pass: true
+    } : {
+      message: () => `expected received to be type ${argument}[]`,
+      pass: false
+    };
+  },
+  toBeTypeOf: (received: object, argument) => {
+    return isType(received, argument) ? {
+      message: () => `expected received to be type ${argument}`,
+      pass: true
+    } : {
+      message: () => `expected received to be type ${argument}[]`,
+      pass: false
+    };
+  },
 });
-test("balanceSheet", async () => {
-  expect(await iex.balanceSheet("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.BalanceSheet)])
-  );
+
+describe('stocks', () => {
+  test("advancedStats", async () => {
+    expect(await iex.stock.advancedStats("AAPL")).toBeInstanceOf(AdvancedStats)
+  });
+  test("balanceSheet", async () => {
+    expect(await iex.stock.balanceSheet("AAPL")).toBeArrayInstanceOf(BalanceSheet)
+  });
+  test("cashFlowStatement", async () => {
+    expect(await iex.stock.cashFlowStatement("AAPL")).toBeArrayInstanceOf(CashFlowStatement)
+  });
+  test("incomeStatement", async () => {
+    expect(await iex.stock.incomeStatement("AAPL")).toBeArrayInstanceOf(IncomeStatement)
+  });
+  test("financials", async () => {
+    expect(await iex.stock.financials("AAPL")).toBeArrayInstanceOf(Financials);
+  });
+  test("book", async () => {
+    expect(await iex.stock.book("AAPL")).toBeInstanceOf(Book);
+  });
+  test("ceoCompensation", async () => {
+    expect(await iex.stock.ceoCompensation("AAPL")).toBeInstanceOf(CeoCompensation);
+  });
+  test("company", async () => {
+    expect(await iex.stock.company("AAPL")).toBeInstanceOf(Company);
+  });
+  test("delayedQuote", async () => {
+    expect(await iex.stock.delayedQuote("AAPL")).toBeInstanceOf(DelayedQuote);
+  });
+  test("dividends", async () => {
+    expect(await iex.stock.dividends("AAPL")).toBeArrayInstanceOf(Dividends);
+  });
+  test("earnings", async () => {
+    expect(await iex.stock.earnings("AAPL")).toBeArrayInstanceOf(Earnings);
+  });
+  test("earningsToday", async () => {
+    expect(await iex.stock.earningsToday("AAPL")).toBeInstanceOf(EarningsToday);
+  });
+  test("estimates", async () => {
+    expect(await iex.stock.estimates("AAPL")).toBeArrayInstanceOf(Estimates);
+  });
+  test("history", async () => {
+    const eod = await iex.stock.history("AAPL", { period: '5d'});
+    expect(eod).toBeArrayInstanceOf(EndOfDay);
+  });
+  test("endOfDay", async () => {
+    expect(await iex.stock.endOfDay("AAPL")).toBeArrayInstanceOf(EndOfDay);
+  });
+  test("endOfDay with date ", async () => {
+    const date = 20190517;
+    expect(await iex.stock.endOfDay("AAPL", { date })).toBeArrayInstanceOf(EndOfDay);
+  });
+  // test("intraday", async () => {
+  //   expect(await iex.stock.intraday("AAPL")).toBeInstanceOf(Intraday);
+  // });
+  test("keyStats", async () => {
+    expect(await iex.stock.keyStats("AAPL")).toBeInstanceOf(KeyStats);
+  });
+  test("list", async () => {
+    expect(await iex.market.list("gainers")).toBeArrayInstanceOf(Quote);
+  });
+  test("logo", async () => {
+    expect(await iex.stock.logoURL("AAPL")).toBeInstanceOf(Logo);
+  });
+  test("news", async () => {
+    expect(await iex.stock.news("AAPL", 2)).toBeArrayInstanceOf(NewsItem);
+  });
+  test("ohlc", async () => {
+    expect(await iex.stock.ohlc("AAPL")).toBeInstanceOf(OHLC);
+  });
+  test("peers", async () => {
+    expect(await iex.stock.peers("AAPL")).toBeArrayTypeOf('string');
+  });
+  test("previousDay", async () => {
+    expect(await iex.stock.previousDay("AAPL")).toBeInstanceOf(PreviousDay);
+  });
+  test("price", async () => {
+    expect(await iex.stock.price("AAPL")).toBeTypeOf('number');
+  });
+  test("priceTarget", async () => {
+    expect(await iex.stock.priceTarget("AAPL")).toBeInstanceOf(PriceTarget);
+  });
+  test("quote", async () => {
+    expect(await iex.stock.quote("AAPL")).toBeInstanceOf(Quote);
+  });
+  test("socialSentiment daily", async () => {
+    expect(await iex.stock.socialSentiment('AAPL', 'daily', "20190213")).toBeInstanceOf(SocialSentiment);
+  });
+  test("socialSentiment minute", async () => {
+    expect(await iex.stock.socialSentiment('AAPL', 'minute', "20190213")).toBeArrayInstanceOf(SocialSentiment);
+  });
+  test("splits", async () => {
+    expect(await iex.stock.splits("CNC")).toBeArrayInstanceOf(Splits);
+  });
+  test("volumeByVenue", async () => {
+    expect(await iex.stock.volumeByVenue("AAPL")).toBeArrayInstanceOf(VolumeByVenue);
+  });
 });
-test("cashFlowStatement", async () => {
-  expect(await iex.cashFlowStatement("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.CashFlowStatement)])
-  );
+describe('crypto', () => {
+  test("cryptoQuote", async () => {
+    expect(await iex.crypto.cryptoQuote("BTCUSD")).toBeInstanceOf(Quote);
+  });
 });
-test("incomeStatement", async () => {
-  expect(await iex.incomeStatement("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.IncomeStatement)])
-  );
+describe('referenceData', () => {
+  test("iexSymbols", async () => {
+    expect(await iex.referenceData.iexSymbols()).toBeArrayInstanceOf(IEXSymbol);
+  });
+  test("marketSymbols", async () => {
+    expect(await iex.referenceData.marketSymbols()).toBeArrayInstanceOf(MarketSymbol);
+  });  
 });
-test("auction", async () => {
-  expect(await iex.auction("AAPL")).toBeInstanceOf(iex.Auction);
+describe('market', () => {
+  test("marketVolume", async () => {
+    expect(await iex.market.marketVolume()).toBeArrayInstanceOf(MarketVolume);
+  });
+  test("sectorPerformance", async () => {
+    expect(await iex.market.sectorPerformance()).toBeArrayInstanceOf(SectorPerformance);
+  });
 });
-test("book", async () => {
-  expect(await iex.book("AAPL")).toBeInstanceOf(iex.Book);
+describe('deep', () => {
+  test("auction", async () => {
+    expect(await iex.deep.auction("AAPL")).toBeInstanceOf(Auction);
+  });
+  test("officialPrice", async () => {
+    expect(await iex.deep.officialPrice("AAPL")).toBeInstanceOf(DEEPOfficialPrice);
+  });
+  test("deepTrades", async () => {
+    expect(await iex.deep.deepTrades("AAPL")).toBeArrayInstanceOf(DEEPTrade);
+  });
 });
-test("ceoCompensation", async () => {
-  expect(await iex.ceoCompensation("AAPL")).toBeInstanceOf(iex.CeoCompensation);
-});
-test("company", async () => {
-  expect(await iex.company("AAPL")).toBeInstanceOf(iex.Company);
-});
-// test("cryptoQuote", async () => {
-//   expect(await iex.cryptoQuote("BTC")).toBeInstanceOf(iex.CryptoQuote);
-// });
-test("officialPrice", async () => {
-  expect(await iex.officialPrice("AAPL")).toBeInstanceOf(iex.DEEPOfficialPrice);
-});
-// test("deepTrades", async () => {
-//   expect(await iex.deepTrades("AAPL")).toEqual(
-//     expect.arrayContaining([expect.any(iex.DEEPTrade)])
-//   );
-// });
-test("delayedQuote", async () => {
-  expect(await iex.delayedQuote("AAPL")).toBeInstanceOf(iex.DelayedQuote);
-});
-// test("dividends", async () => {
-//   expect(await iex.dividends("AAPL")).toEqual(
-//     expect.arrayContaining([expect.any(iex.Dividends)])
-//   );
-// });
-test("earnings", async () => {
-  expect(await iex.earnings("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.Earnings)])
-  );
-});
-// test("earningsToday", async () => {
-//   expect(await iex.earningsToday("AAPL")).toEqual(
-//     expect.arrayContaining([expect.any(iex.EarningsToday)])
-//   );
-// });
-// test("effectiveSpread", async () => {
-//   expect(await iex.effectiveSpread("AAPL")).toEqual(
-//     expect.arrayContaining([expect.any(iex.EffectiveSpread)])
-//   );
-// });
-test("endOfDay", async () => {
-  expect(await iex.endOfDay("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.EndOfDay)])
-  );
-});
-test("endOfDayCloseOnly", async () => {
-  expect(await iex.endOfDayCloseOnly("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.EndOfDayCloseOnly)])
-  );
-});
-test("estimates", async () => {
-  expect(await iex.estimates("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.Estimates)])
-  );
-});
-test("financials", async () => {
-  expect(await iex.financials("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.Financials)])
-  );
-});
-test("history EndOfDay", async () => {
-  const eod = await iex.history("AAPL");
-  // console.log(eod.slice(0,2))  
-  expect(eod).toEqual(
-    expect.arrayContaining([expect.any(iex.EndOfDay)])
-  )
-});
-test("history Intraday", async () => {
-  const intraday = await iex.history("AAPL",{period: "1d"})
-  // console.log(intraday.slice(0,2))
-  expect(intraday).toEqual(
-    expect.arrayContaining([expect.any(iex.Intraday)])
-  )
-});
-test("history Intraday for date ", async () => {
-  const intraday = await iex.history("AAPL",{date: "2019-05-17"})
-  // console.log(intraday.slice(0,2))
-  expect(intraday).toEqual(
-    expect.arrayContaining([expect.any(iex.Intraday)])
-  )
-});
-test("history EndOfDay for date ", async () => {
-  const intraday = await iex.history("AAPL",{chartByDay:true,date: "2019-05-17"})
-  // console.log(intraday.slice(0,2))
-  expect(intraday).toEqual(
-    expect.arrayContaining([expect.any(iex.EndOfDay)])
-  )
-});
-test("iexSymbols", async () => {
-  expect(await iex.iexSymbols()).toEqual(
-    expect.arrayContaining([expect.any(iex.IEXSymbol)])
-  );
-});
-test("incomeStatement", async () => {
-  expect(await iex.incomeStatement("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.IncomeStatement)])
-  );
-});
-test("intraday", async () => {
-  expect(await iex.intraday("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.Intraday)])
-  );
-});
-test("intradayIEXOnly", async () => {
-  expect(await iex.intradayIEXOnly("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.IntradayIEXOnly)])
-  );
-});
-test("intradayForDate", async () => {
-  expect(await iex.intradayForDate("AAPL","20190517")).toEqual(
-    expect.arrayContaining([expect.any(iex.IntradayIEXOnly)])
-  );
-});
-test("keyStats", async () => {
-  expect(await iex.keyStats("AAPL")).toBeInstanceOf(iex.KeyStats);
-});
-// test("list", async () => {
-//   expect(await iex.list("gainers")).toEqual(
-//     expect.arrayContaining([expect.any(iex.Quote)])
-//   );
-// });
-test("logo", async () => {
-  expect(await iex.logoURL("AAPL")).toBeInstanceOf(iex.Logo);
-});
-test("marketSymbols", async () => {
-  expect(await iex.marketSymbols()).toEqual(
-    expect.arrayContaining([expect.any(iex.MarketSymbol)])
-  );
-});
-test("marketVolume", async () => {
-  expect(await iex.marketVolume()).toEqual(
-    expect.arrayContaining([expect.any(iex.MarketVolume)])
-  );
-});
-test("news", async () => {
-  expect(await iex.news("AAPL", 2)).toEqual(
-    expect.arrayContaining([expect.any(iex.NewsItem)])
-  );
-});
-test("ohlc", async () => {
-  expect(await iex.ohlc("AAPL")).toEqual(
-    expect.any(iex.OHLC)
-  );
-});
-// test("peers", async () => {
-//   expect(await iex.peers("AAPL")).toEqual(
-//     expect.arrayContaining([expect.any(String)])
-//   );
-// });
-test("previousDay", async () => {
-  expect(await iex.previousDay("AAPL")).toBeInstanceOf(iex.PreviousDay);
-});
-test("price", async () => {
-  expect(await iex.price("AAPL")).toEqual(expect.any(Number));
-});
-test("priceTarget", async () => {
-  expect(await iex.priceTarget("AAPL")).toBeInstanceOf(iex.PriceTarget);
-});
-test("quote", async () => {
-  expect(await iex.quote("AAPL")).toBeInstanceOf(iex.Quote);
-});
-test("sectorPerformance", async () => {
-  expect(await iex.sectorPerformance()).toEqual(
-    expect.arrayContaining([expect.any(iex.SectorPerformance)])
-  );
-});
-test("socialSentiment daily", async () => {
-  expect(await iex.socialSentiment("AAPL", "daily", "20190213")).toBeInstanceOf(iex.SocialSentiment);
-});
-test("socialSentiment minute", async () => {
-  expect(await iex.socialSentiment("AAPL", "daily", "20190213")).toBeInstanceOf(iex.SocialSentiment);
-});
-// test("splits", async () => {
-//   expect(await iex.splits("CNC")).toEqual(
-//     expect.arrayContaining([expect.any(iex.Splits)])
-//   );
-// });
-// test("tops", async () => {
-//   expect(await iex.tops("AAPL")).toEqual(
-//     expect.arrayContaining([expect.any(iex.TOPS)])
-//   );
-// });
-test("topsLast", async () => {
-  expect(await iex.topsLast("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.TOPSLast)])
-  );
-});
-test("volumeByVenue", async () => {
-  expect(await iex.volumeByVenue("AAPL")).toEqual(
-    expect.arrayContaining([expect.any(iex.VolumeByVenue)])
-  );
-});
+describe('tops', () => {
+  test("tops", async () => {
+    expect(await iex.tops.tops("A")).toBeArrayInstanceOf(TOPS);
+  });
+  test("topsLast", async () => {
+    expect(await iex.tops.topsLast("A")).toBeArrayInstanceOf(TOPSLast);
+  });
+})
